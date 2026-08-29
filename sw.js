738b6386912242ef0dd1d0817f8a46aa4342ca8d
Terr-cpu <monk3y.bussin3ss@gmail.com>
2026-08-29 22:34:02 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,41 @@
+/* Service worker mínimo: cachea el "shell" para que la app abra sin conexión.
+   Los datos siempre van a Google Drive por red; aquí NO se cachea nada de Google. */
+const CACHE = 'hourglass-v3';
+const SHELL = [
+  './',
+  './index.html',
+  './app.js',
+  './style.css',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(SHELL)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys()
+      .then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  const url = new URL(e.request.url);
+  if (e.request.method !== 'GET' || url.origin !== self.location.origin) return; // deja pasar Drive, Google, CDNs
+  e.respondWith(
+    caches.match(e.request).then((hit) => {
+      const net = fetch(e.request).then((res) => {
+        if (res && res.ok) {
+          const copy = res.clone();
+          caches.open(CACHE).then((c) => c.put(e.request, copy));
+        }
+        return res;
+      }).catch(() => hit);
+      return hit || net;
+    })
+  );
+});
